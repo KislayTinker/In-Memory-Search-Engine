1e880ac70076457e56ffbec26771b270ce6d1bdf
Kislay Tinker <kislaytinker@gmail.com>
2026-01-05 12:22:13 +0530

    Refactor main method to use searchEngine

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.List;
+
 public class Main {
-  public static void main(String args[]) {
-    searching search = new searching();
-  }
+    public static void main(String[] args) {
+
+        searchEngine engine = new searchEngine();
+
+        engine.addDocument("Java search engine");
+        engine.addDocument("Python search engine");
+        engine.addDocument("Java programming");
+        engine.addDocument("Advanced Java search techniques");
+
+        List<searchEngine.Result> results = engine.topKTfIdfBoolean("Java AND search NOT python", 5);
+
+        for (searchEngine.Result result : results) {
+            System.out.println("Doc " + result.docId + " -> score: " + result.score);
+        }
+      
+    }
 }
